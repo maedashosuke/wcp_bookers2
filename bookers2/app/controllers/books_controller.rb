8ea728d74ec8430f962ad750bb26de52081ed64a
maedashosuke <shosukemaeda.1011@gmail.com>
2020-11-20 11:26:17 +0000

    [Update] 課題

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,12 +1,23 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new
   end
 
   def create
+    @books = Book.all
+  	@book = Book.new(book_params)
+  	@book.user_id = current_user.id
+  	if @book.save
+  	   flash[:notice] = "You have creatad book successfully."
+  	   redirect_to book_path(@book.id)
+  	else
+  	   render :index
+  	end
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -19,6 +30,12 @@ class BooksController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 
 end
--- bookers2/db/migrate/20201120095229_create_books.rb
+++ bookers2/db/migrate/20201120095229_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.text :title_name
+      t.text :title
       t.string :profile_image_id
-      t.text :caption
+      t.text :body
       t.integer :user_id
 
       t.timestamps
