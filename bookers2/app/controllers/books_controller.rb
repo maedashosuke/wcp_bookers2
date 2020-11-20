90c12182753febb07dd845bfe0559906f3b6d184
maedashosuke <shosukemaeda.1011@gmail.com>
2020-11-20 10:22:22 +0000

    [Update] 課題

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+end
