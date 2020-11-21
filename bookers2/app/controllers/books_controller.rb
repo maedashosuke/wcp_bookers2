bff0058873738c47e47af0bcf27520e4ec9dc01d
maedashosuke <shosukemaeda.1011@gmail.com>
2020-11-21 06:39:39 +0000

    [Update] 課題

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -10,9 +10,9 @@ class BooksController < ApplicationController
   	@book.user_id = current_user.id
   	if @book.save
   	   flash[:notice] = "You have creatad book successfully."
-  	   redirect_to book_path(@book.id)
+  	   redirect_to books_path(@book.id)
   	else
-  	   render :index
+  	   render :new
   	end
   end
 
