7dae873965ee9d1b7d60fbd0868da60b99cdf20c
maedashosuke <shosukemaeda.1011@gmail.com>
2020-11-21 04:02:46 +0000

    [Update] 課題

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_110307) do
+ActiveRecord::Schema.define(version: 2020_11_21_033410) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_110307) do
     t.string "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
