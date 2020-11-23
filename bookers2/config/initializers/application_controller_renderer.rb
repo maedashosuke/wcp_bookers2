139f815f30bbb528a2642128d06ca50411049b63
maedashosuke <shosukemaeda.1011@gmail.com>
2020-11-23 06:32:30 +0000

    [Update] 課題

--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b673c4e03b060cd7fd3cf48605e6e6f73fdc79a341c2b6aef721d0614cad0bdf88f7f39945b9c057144c03cb0f5fc2a701f0f5616b71ea091028b16a0dfdd861'
